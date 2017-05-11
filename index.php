9f7375453306a2ac1e71b1ae0c8c4fb7932353a9
jack514318831 <gang.he@gmx.de>
2017-05-11 14:49:36 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+
+<?php
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Berlitz Online-Tool</title>
+<link rel="stylesheet" type="text/css" href="css/styles.css" >
+</head>
+
+<body>
+<div id="header">
+  <div id="header-inner">
+  <div id="logo"><img src="img/logo.jpg" alt="Berlitz" title="Berlitz" /></div>
+  <div id="page-headline">&nbsp;</div>
+  </div>
+</div>
+<div id="wrapper">
+<div id="header-image"><img src="img/header.jpg"/></div>
+
+  <h1>Die Berlitz Lernstilanalyse</h1>
+ Herzlich willkommen bei der Berlitz Lernstilanalyse. Durch diese Kurzbefragung erfahren Sie mehr zu Ihrem persönlichen Lernstil bzw. Lerntypen. Bei Berlitz wollen wir nicht nur Ihrem Sprachniveau gerecht werden, sondern auch für Sie eine optimale Lernumgebung schaffen um ideal auf Ihre individuellen Bedürfnisse eingehen zu können. Diese kurze Befragung hilft Ihnen und uns dabei.
+
+ <br />
+ <br />
+ Auf den folgenden Seiten bitten wir Sie, insgesamt 23 kurze Fragen zu Ihren Präferenzen beim Sprachen lernen zu beantworten. Die Befragung dauert ca. 5 Minuten und ist völlig anonym. <br />
+<br />
+Ihren Ergebnisbericht mit Informationen und hilfreichen Tipps zu Ihrem Lerntyp erhalten Sie unmittelbar nach dem Absenden Ihrer Angaben.
+
+<br />
+<br />
+Wir wünschen Ihnen viel Erfolg beim Ausfüllen der folgenden Seiten.<br />
+<br />
+<form action= "page01.php" name= "form1">
+<input type ="submit" name="submit" value = "Weiter" class="btn">
+</form>
+<div id="footer">
+  <div id="footer-inner-home">
+  <div id="footer-left">Mit Unterstützung der EuPD Research Sustainable Management GmbH. </div>
+ 
+  <div id="footer-right"><a href="impressum.html">Impressum</a></div>
+   </div></div>
+</div>
+</body>
+</html>
